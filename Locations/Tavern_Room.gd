d93b3d47bd63dbcd236009b3a7a10170761e6cea
Shapovalov <demanxd>
2020-09-21 16:57:21 +0300

    some trying for dialog system

--- Locations/Tavern_Room.gd
+++ Locations/Tavern_Room.gd
@@ -1,24 +1,27 @@
 extends Node2D
 
 onready var TIMER_STATE = true
+onready var TEXT_SHOWING = false
+onready var TIMER_SET_ONCE = false
 
 onready var Timer = $Timer
+onready var MCF1  = $YSort/MCFriend1
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 #timerblock================
 func set_timer(s):
+	print_debug("setting timer")
 	Timer.set_wait_time(s)
 	Timer.set_one_shot(true)
 	Timer.start()
 
 
 func _on_Timer_timeout():
-	print_debug("called")
+	print_debug("called + Timer state = ", TIMER_STATE)
 	if TIMER_STATE:
 		timer_true()
 	else:
@@ -29,7 +32,25 @@ func _on_Timer_timeout():
 
 func timer_false():
 	TIMER_STATE = true
+	TEXT_SHOWING = true
+	TIMER_SET_ONCE = false
 
 
 func timer_true():
 	TIMER_STATE = false
+	TEXT_SHOWING = false
+	TIMER_SET_ONCE = false
+
+
+func _process(delta):
+	if !TEXT_SHOWING:
+		if !TIMER_SET_ONCE:
+			set_timer(2)
+			MCF1.set_text("Hello there!")
+			MCF1.show_text()
+			TIMER_SET_ONCE = true
+	else:
+		if !TIMER_SET_ONCE:
+			set_timer(2)
+			MCF1.hide_text()
+			TIMER_SET_ONCE = true
