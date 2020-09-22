f2409b54d620080c2dbf3e622ae7c284f786db01
Shapovalov <demanxd>
2020-09-22 17:20:35 +0300

    sketches for dialogue system

---
+++ DialogueAction.gd
@@ -0,0 +1,30 @@
+extends Node
+#http://gameprogrammingpatterns.com/observer.html
+
+# Wait 5 seconds, then resume execution.
+#yield(get_tree().create_timer(5.0), "timeout")
+
+class_name Dialogue_Action
+
+export (String, FILE, "*.json") var dialogue_file_path : String
+
+func interact() -> void:
+	var dialogue : Dictionary = load_dialogue(dialogue_file_path)
+	#yield(local_map.play_dialogue(dialogue), "complited")
+	emit_signal("finished")
+
+
+func load_dialogue(file_path) -> Dictionary:
+	#===
+	#Parses a JSON file and returns it as a dictionary
+	#===
+	
+	var file = File.new()
+	#assert 
+	file.file_exists(file_path)
+	
+	file.open(file_path, file.READ)
+	var dialogue = parse_json(file.get_as_text())
+	#assert 
+	dialogue.size() > 0
+	return dialogue
