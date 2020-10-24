b7a8cb4de7cd3c1958b63f87256fbeab17ccb93d
Shapovalov <demanxd>
2020-10-24 16:46:44 +0300

    Some cleaning code. Deleted unused functions with timers

--- Locations/Tavern_Room.gd
+++ Locations/Tavern_Room.gd
@@ -34,50 +34,5 @@ func _ready():
 		if NPC_list.get_node(person.name) != null:
 			NPC_list.get_node(person.name).hide_text()
 	
-	print("last sentence was reached")
+	print_debug("last sentence was reached")
 	
-
-
-#timerblock================
-func set_timer(s):
-#	print_debug("setting timer")
-	Timer.set_wait_time(s)
-	Timer.set_one_shot(true)
-	Timer.start()
-
-
-func _on_Timer_timeout():
-#	print_debug("called + Timer state = ", TIMER_STATE)
-	if TIMER_STATE:
-		timer_true()
-	else:
-		timer_false()
-		
-#timerblock================
-
-
-func timer_false():
-	TIMER_STATE = true
-	TEXT_SHOWING = true
-	TIMER_SET_ONCE = false
-
-
-func timer_true():
-	TIMER_STATE = false
-	TEXT_SHOWING = false
-	TIMER_SET_ONCE = false
-
-
-func _process(delta):
-#	if !TEXT_SHOWING:
-#		if !TIMER_SET_ONCE:
-#			set_timer(2)
-#			MCF1.set_text("Hello there!")
-#			MCF1.show_text()
-#			TIMER_SET_ONCE = true
-#	else:
-#		if !TIMER_SET_ONCE:
-#			set_timer(2)
-#			MCF1.hide_text()
-#			TIMER_SET_ONCE = true
-	pass
