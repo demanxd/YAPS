e76dbc928a02ae14b5cfad467fbe8ce59cb9f809
Shapovalov <demanxd>
2020-10-31 12:02:04 +0300

    Fixed falling for NPC Characters Fixed collision for Citizen1 and Citizen2 (for tests) Updated Test_room

--- NPC.gd
+++ NPC.gd
@@ -13,18 +13,30 @@ func _physics_process(_delta):
 	var direction = get_direction()
 	var snap_vector = Vector2.DOWN * FLOOR_DETECT_DISTANCE if direction.y == 0.0 else Vector2.ZERO
 	var is_on_platform = platform_detector.is_colliding()
+	var is_jump_interrupted = false
 	
 	if not is_on_platform:
-		_velocity.y *= 0.6
-#		print_debug(self.name, " WARNING: Not on platform!");
-	
-	if direction.y != 0.0:
-		_velocity.y = speed.y * direction.y
+		_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
 	
 	_velocity = move_and_slide_with_snap(
 		_velocity, snap_vector, FLOOR_NORMAL, not is_on_platform, 4, 0.9, false
 		)
 
+func calculate_move_velocity(
+		linear_velocity,
+		direction,
+		speed, 
+		is_jump_interrupted
+	):
+	var velocity = linear_velocity
+	velocity.x = speed.x * direction.x
+	if direction.y != 0.0:
+		velocity.y = speed.y * direction.y
+	if is_jump_interrupted:
+		# Decrease the Y velocity by multiplying it, but don't set it to 0
+		# as to not be too abrupt.
+		velocity.y *= 0.6
+	return velocity
 
 func get_direction():
 	return Vector2(
